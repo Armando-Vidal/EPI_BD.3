600dfaadbc5bc4306b7756ae6f0744942541848a
pinheiro-lu <luanpereira0603@gmail.com>
2024-01-04 16:32:41 -0300

    Conecta ao banco de dados

---
+++ nodeapp/database.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'nodeapp'
+});
+connection.connect(function(err) {
+    if (err) throw err;
+});
+console.log("Conectado ao banco de dados!");
+module.exports = connection;
